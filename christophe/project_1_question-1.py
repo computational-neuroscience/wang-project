652d9f2a31bcb282d2a2a39899ac7b0f4c48304e
ChristopheBossens <christophe.bossens@gmail.com>
2017-07-27 09:47:13 +0200

    Updated with reaction times

--- christophe/project_1_question-1.py
+++ christophe/project_1_question-1.py
@@ -14,6 +14,7 @@ Created on Thu Jul  6 10:33:09 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.stats.proportion import proportion_confint as binofit
 
 sns.set(font_scale=1.3)
 ## Input-output curve
@@ -90,7 +91,7 @@ def run_simulation(params={'mu_1':30,'mu_2':30}):
         results[3,t] = s_2
     return results
     
-results = run_simulation({'mu_1':30,'mu_2':30})
+results = run_simulation({'mu_1':60,'mu_2':00})
 plt.subplot(121)
 plt.plot(results[0,:])
 plt.plot(results[1,:])
@@ -130,14 +131,14 @@ for i, c in enumerate(c_prime):
     mu_2 = mu_0*(1 - c)
     mu[0,i] = mu_1
     mu[1,i] = mu_2
-plt.plot(c_prime,mu[0,:])
-plt.plot(c_prime,mu[1,:])
+plt.plot(np.log(c_prime),mu[0,:])
+plt.plot(np.log(c_prime),mu[1,:])
 plt.legend([r'$\mu_1$',r'$\mu_2$'])
 plt.xlabel('coherence')
 plt.title('Input as a function of coherence')
 
 #%% Simulate with different coherence values
-n_runs = 100
+n_runs = 200
 wins = np.zeros(len(c_prime))
 
 for i, c in enumerate(c_prime):
@@ -149,4 +150,62 @@ for i, c in enumerate(c_prime):
         if sim_result[0,1500] > sim_result[1,1500]:
             wins[i] += 1
 
-plt.plot(np.log(c_prime),wins/n_runs)
+y = wins/n_runs
+err = binofit(wins,n_runs,0.05)
+plt.title('Psychometric curve')
+plt.errorbar(np.log(c_prime),y,yerr = [y-err[0],err[1]-y],fmt='.-')
+plt.xlabel(r"$\log{ (c')}$")
+plt.ylabel('P(correct)')
+plt.ylim(0.5,1.05)
+
+#%% Reaction time task
+n_runs = 200
+example_curves = np.zeros([2,len(c_prime), total_steps])
+rt = np.zeros([2,len(c_prime),n_runs])
+
+y_1 = np.zeros(len(c_prime))
+y_2 = np.zeros(len(c_prime))
+y_3 = np.zeros(len(c_prime))
+y_4 = np.zeros(len(c_prime))
+
+threshold = 15
+for i,c in enumerate (c_prime):
+    mu_1 = mu_0*(1 + c)
+    mu_2 = mu_0*(1 - c)
+    
+    wins = 0
+    for j in range(n_runs):
+        sim_result = run_simulation({'mu_1': mu_1, 'mu_2' : mu_2})
+        
+        if len(np.where(sim_result[0,:]>threshold))>0:
+            threshold_time_1 = np.argmax(sim_result[0,:] > threshold)
+        else:
+            threshold_time_1 = total_steps
+            
+        if len(np.where(sim_result[1,:]>threshold)[0])>0:
+            threshold_time_2 = np.argmax(sim_result[1,:] > threshold)
+        else:
+            threshold_time_2 = total_steps
+            
+        if threshold_time_1 < threshold_time_2:
+            rt[0,i,j] = 0
+            rt[1,i,j] = threshold_time_1
+        else:
+            rt[0,i,j] = 1
+            rt[1,i,j] = threshold_time_2     
+              
+    y_1[i] = np.mean(rt[1,i, rt[0,i,:]==0])
+    y_2[i] = np.mean(rt[1,i, rt[0,i,:]==1])
+    y_3[i] = np.mean(rt[1,i,:])
+    y_4[i] = len(np.where(rt[0,i,:] == 0)[0])/float(n_runs)
+    
+plt.plot(np.log(c_prime), y_1)
+plt.plot(np.log(c_prime), y_2)
+plt.plot(np.log(c_prime), y_3,'--k')
+plt.legend(['Population #1','Population #2'])
+
+plt.plot(np.log(c_prime),y_4)
+
+plt.plot(sim_result[0,:])
+plt.plot(sim_result[1,:])
+plt.axhline(15)
